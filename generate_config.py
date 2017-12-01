c26282f123489c6322dc4cf085bcdd55d9fd6556
Christian Rachmaninoff <rachman1@cs.utah.edu>
2017-12-01 16:30:45 -0700

    adding config generation script

---
+++ generate_config.py
@@ -0,0 +1,7 @@
+params = {
+    "softmax_to_relu" : ["True", "False"],
+    "maxpool_type" : ['fir_max', 'exp_max', 'avg_max'],
+    "max2avg_pool" : ["True", "False"],
+    "spike_code" : ["temporal_mean_rate", "temporal_pattern"],
+    "num_bits" : ["8", "16", "32", "64"]
+}
