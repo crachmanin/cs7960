716f07cc637d26b8247bd2fff066724098872d6e
Christian Rachmaninoff <rachman1@cs.utah.edu>
2017-12-03 19:16:26 -0700

    modifying scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-for d in configs/* ; do
+for d in configs/config$1* ; do
+	#echo $d
 	( cd $d && cp ../../mnist_mlp.h5 . && snntoolbox -t $(basename $d)) 
 done
---
+++ run_all.sh
@@ -0,0 +1,3 @@
+for i in {0..5..1}; do
+	./run.sh $i &
+done
